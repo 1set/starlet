2c24f64d0f1706f6df3f062de555a820134c78d3
Hyori 효리 <hyorigo@icloud.com>
2023-06-07 15:11:50 +0800

    For modules

---
+++ module.go
@@ -0,0 +1,29 @@
+package starlet
+
+// ModuleName represents a Starlark module name or collection of functions and values.
+type ModuleName string
+
+const (
+	// ModuleGoIdiomatic is a collection of Go idiomatic functions and values. e.g. true, false, nil, exit, etc.
+	ModuleGoIdiomatic = ModuleName("go_idiomatic")
+
+	// ModuleStruct is the official Starlark struct and module.
+	ModuleStruct = ModuleName("struct")
+
+	// ModuleTime is the official Starlark time module.
+	ModuleTime = ModuleName("time")
+
+	// ModuleMath is the official Starlark math module.
+	ModuleMath = ModuleName("math")
+
+	// ModuleJSON is the official Starlark JSON module.
+	ModuleJSON = ModuleName("json")
+)
+
+// ModuleNameList is a list of Starlark module names.
+type ModuleNameList []ModuleName
+
+// Clone returns a copy of the list.
+func (l ModuleNameList) Clone() []ModuleName {
+	return append([]ModuleName{}, l...)
+}
