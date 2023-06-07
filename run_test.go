fd09685f305ec9a506d2ded1b9cf7a029833080c
Hyori 효리 <hyorigo@icloud.com>
2023-06-07 21:55:25 +0800

    For more testing

--- run_test.go
+++ run_test.go
@@ -4,7 +4,10 @@ import (
 	"context"
 	"os"
 	"starlet"
+	"strings"
 	"testing"
+
+	"go.starlark.net/starlark"
 )
 
 func expectErr(t *testing.T, err error, expected string) {
@@ -16,17 +19,63 @@ func expectErr(t *testing.T, err error, expected string) {
 	}
 }
 
-func Test_EmptyMachine_RunNoCode(t *testing.T) {
+// getPrintCompareFunc returns a print function and a compare function.
+func getPrintCompareFunc(t *testing.T) (starlet.PrintFunc, func(s string)) {
+	var sb strings.Builder
+	return func(thread *starlark.Thread, msg string) {
+			sb.WriteString(msg)
+			sb.WriteString("\n")
+		}, func(exp string) {
+			act := sb.String()
+			if act != exp {
+				t.Errorf("expected print %q, got %q", exp, act)
+			}
+		}
+}
+
+func Test_EmptyMachine_Run_NoCode(t *testing.T) {
 	m := starlet.NewEmptyMachine()
 	// run with empty script
 	_, err := m.Run(context.Background())
 	expectErr(t, err, `starlet: run: no code to run`)
 }
 
-func Test_EmptyMachine_RunNoSpecificCode(t *testing.T) {
+func Test_EmptyMachine_Run_NoSpecificFile(t *testing.T) {
 	m := starlet.NewEmptyMachine()
 	m.SetScript("", nil, os.DirFS("example"))
 	// run with no specific file name
 	_, err := m.Run(context.Background())
 	expectErr(t, err, `starlet: run: no code to run`)
+}
+
+func Test_EmptyMachine_Run_APlusB(t *testing.T) {
+	m := starlet.NewEmptyMachine()
+	code := `a = 1 + 2`
+	m.SetScript("a_plus_b.star", []byte(code), nil)
+	out, err := m.Run(context.Background())
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+	if out == nil {
+		t.Errorf("unexpected nil output")
+	} else if out["a"] != int64(3) {
+		t.Errorf("unexpected output: %v", out)
+	}
+}
+
+func Test_EmptyMachine_Run_HelloWorld(t *testing.T) {
+	m := starlet.NewEmptyMachine()
+	// set print function
+	printFunc, cmpFunc := getPrintCompareFunc(t)
+	m.SetPrintFunc(printFunc)
+	// set code
+	code := `print("Aloha kāua!")`
+	m.SetScript("aloha.star", []byte(code), nil)
+	// run
+	_, err := m.Run(context.Background())
+	if err != nil {
+		t.Errorf("unexpected error: %v", err)
+	}
+	// compare
+	cmpFunc("Aloha kāua!\n")
 }
