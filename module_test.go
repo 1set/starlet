212211cac0c850a1151b0640e1c8111bbeac031b
Hyori 효리 <hyorigo@icloud.com>
2023-06-09 17:50:34 +0800

    For module list tests

--- module_test.go
+++ module_test.go
@@ -1,8 +1,11 @@
 package starlet_test
 
 import (
+	"reflect"
 	"starlet"
 	"testing"
+
+	"go.starlark.net/starlark"
 )
 
 var (
@@ -40,6 +43,65 @@ func TestGetBuiltinModule(t *testing.T) {
 				t.Errorf("Expected module %q, got nil", tt.name)
 			} else if !tt.found && got != nil {
 				t.Errorf("Expected nil, got module %q", tt.name)
+			}
+		})
+	}
+}
+
+func Test_ModuleLoaderList_Clone(t *testing.T) {
+	moduleLoaderList := starlet.ModuleLoaderList{starlet.GetBuiltinModule("go_idiomatic"), starlet.GetBuiltinModule("struct")}
+
+	clone := moduleLoaderList.Clone()
+	if len(clone) != len(moduleLoaderList) {
+		t.Errorf("Expected clone length %d, got %d", len(moduleLoaderList), len(clone))
+	}
+	for i := range clone {
+		if reflect.DeepEqual(clone[i], moduleLoaderList[i]) {
+			t.Errorf("Expected different clone at index %d to be %T, got %T", i, moduleLoaderList[i], clone[i])
+		}
+	}
+}
+
+func Test_ModuleLoaderList_LoadAll(t *testing.T) {
+	_, failLoader := getErrorModuleLoader()
+	tests := []struct {
+		name          string
+		moduleLoaders starlet.ModuleLoaderList
+		dict          starlark.StringDict
+		wantErr       string
+	}{
+		{
+			name:          "valid modules",
+			moduleLoaders: starlet.ModuleLoaderList{starlet.GetBuiltinModule("go_idiomatic"), starlet.GetBuiltinModule("struct")},
+			dict:          make(starlark.StringDict),
+		},
+		{
+			name:          "nil dict",
+			moduleLoaders: starlet.ModuleLoaderList{starlet.GetBuiltinModule("go_idiomatic"), starlet.GetBuiltinModule("struct")},
+			dict:          nil,
+			wantErr:       "starlet: cannot load modules into nil dict",
+		},
+		{
+			name:          "nil module loader",
+			moduleLoaders: starlet.ModuleLoaderList{starlet.GetBuiltinModule("go_idiomatic"), nil},
+			dict:          make(starlark.StringDict),
+			wantErr:       "starlet: nil module loader",
+		},
+		{
+			name:          "invalid module",
+			moduleLoaders: starlet.ModuleLoaderList{starlet.GetBuiltinModule("go_idiomatic"), failLoader},
+			dict:          make(starlark.StringDict),
+			wantErr:       "starlet: failed to load module: invalid module loader",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.moduleLoaders.LoadAll(tt.dict)
+			if tt.wantErr != "" {
+				expectErr(t, err, tt.wantErr)
+			} else if err != nil {
+				t.Errorf("Expected no error, got '%v'", err)
 			}
 		})
 	}
