37d0471170f4275d5df26c0b5443d900978f9024
Hyori 효리 <hyorigo@icloud.com>
2023-06-11 02:14:07 +0800

    For make file

--- module_test.go
+++ module_test.go
@@ -245,3 +245,54 @@ func Test_MakeBuiltinModuleLoaderMap(t *testing.T) {
 		})
 	}
 }
+
+func TestModuleLoaderFromString(t *testing.T) {
+	tests := []struct {
+		name        string
+		fileName    string
+		source      string
+		predeclared starlark.StringDict
+		wantKeys    []string
+		wantErr     string
+	}{
+		{
+			name:        "empty filename",
+			fileName:    "",
+			source:      "a = 1",
+			predeclared: map[string]starlark.Value{"b": starlark.MakeInt(2)},
+			wantKeys:    []string{"a"},
+		},
+		{
+			name:        "empty source",
+			fileName:    "test.star",
+			source:      "",
+			predeclared: map[string]starlark.Value{"b": starlark.MakeInt(2)},
+			wantKeys:    []string{},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			// make and run the module
+			loader := starlet.ModuleLoaderFromString(tt.fileName, tt.source, tt.predeclared)
+			mod, err := loader()
+			if tt.wantErr != "" {
+				expectErr(t, err, tt.wantErr)
+			}
+			// check the module result
+			la := len(mod)
+			le := len(tt.wantKeys)
+			if la != le {
+				t.Errorf("Expected module has %d keys, got %d", le, la)
+				return
+			}
+			if len(tt.wantKeys) != 0 {
+				for _, key := range tt.wantKeys {
+					if _, ok := mod[key]; !ok {
+						t.Errorf("Expected module to contain key %q, got: %v", key, mod)
+						return
+					}
+				}
+			}
+		})
+	}
+}
