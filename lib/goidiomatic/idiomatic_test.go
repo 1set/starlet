3ab67346d3ae12db28c0d473c730053516dc3058
Hyori 효리 <hyorigo@icloud.com>
2023-06-14 20:16:42 +0800

    For test in goidiomic

---
+++ lib/goidiomatic/idiomatic_test.go
@@ -0,0 +1,137 @@
+package goidiomatic_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/1set/starlet/lib/goidiomatic"
+	itn "github.com/1set/starlet/lib/internal"
+)
+
+func TestLoadModule_GoIdiomatic(t *testing.T) {
+	header := `load('assert.star', 'assert')`
+	tests := []struct {
+		name    string
+		script  string
+		wantErr error
+	}{
+		{
+			name: `boolean`,
+			script: `
+load('go_idiomatic', 'true', 'false')
+assert.eq(true, True)
+assert.eq(false, False)
+`,
+		},
+		{
+			name: `nil`,
+			script: `
+load('go_idiomatic', 'nil')
+assert.eq(nil, None)
+`,
+		},
+		{
+			name: `sleep`,
+			script: `
+load('go_idiomatic', 'sleep')
+sleep()
+`,
+			wantErr: errors.New(`sleep: missing argument for secs`),
+		},
+		{
+			name: `sleep 0`,
+			script: `
+load('go_idiomatic', 'sleep')
+sleep(0)
+`,
+		},
+		{
+			name: `sleep 10ms`,
+			script: `
+load('go_idiomatic', 'sleep')
+sleep(0.01)
+`,
+		},
+		{
+			name: `sleep negative`,
+			script: `
+load('go_idiomatic', 'sleep')
+sleep(-1)
+`,
+			wantErr: errors.New(`secs must be non-negative`),
+		},
+		{
+			name: `sleep hello`,
+			script: `
+load('go_idiomatic', 'sleep')
+sleep('hello')
+`,
+			wantErr: errors.New(`sleep: for parameter secs: got string, want float or int`),
+		},
+		{
+			name: `sleep none`,
+			script: `
+load('go_idiomatic', 'sleep')
+sleep(None)
+`,
+			wantErr: errors.New(`sleep: for parameter secs: got NoneType, want float or int`),
+		},
+		{
+			name: `exit`,
+			script: `
+load('go_idiomatic', 'exit')
+exit()
+`,
+			wantErr: errors.New(`starlet runtime system exit`),
+		},
+		{
+			name: `exit 0`,
+			script: `
+load('go_idiomatic', 'exit')
+exit(0)
+`,
+			wantErr: errors.New(`starlet runtime system exit`),
+		},
+		{
+			name: `exit 1`,
+			script: `
+load('go_idiomatic', 'exit')
+exit(1)
+`,
+			wantErr: errors.New(`starlet runtime system exit`),
+		},
+		{
+			name: `exit -1`,
+			script: `
+load('go_idiomatic', 'exit')
+exit(-1)
+`,
+			wantErr: errors.New(`exit: for parameter code: -1 out of range (want value in unsigned 8-bit range)`),
+		},
+		{
+			name: `exit hello`,
+			script: `
+load('go_idiomatic', 'exit')
+exit('hello')
+`,
+			wantErr: errors.New(`exit: for parameter code: got string, want int`),
+		},
+		{
+			name: `quit`,
+			script: `
+load('go_idiomatic', 'quit')
+quit()
+`,
+			wantErr: errors.New(`starlet runtime system exit`),
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			res, err := itn.ExecModuleTestScript(t, goidiomatic.ModuleName, goidiomatic.LoadModule, header+"\n"+tt.script, tt.wantErr)
+			if (err != nil) != (tt.wantErr != nil) {
+				t.Errorf("ExecModuleTestScript(%q) expects error = '%v', actual error = '%v', result = %v", tt.name, tt.wantErr, err, res)
+				return
+			}
+		})
+	}
+}
--- lib/internal/testloader.go
+++ lib/internal/testloader.go
@@ -2,11 +2,12 @@ package internal
 
 import (
 	"fmt"
-	"go.starlark.net/starlark"
-	"go.starlark.net/starlarktest"
 	"path/filepath"
 	"runtime"
 	"testing"
+
+	"go.starlark.net/starlark"
+	"go.starlark.net/starlarktest"
 )
 
 // ModuleLoadFunc is a function that loads a Starlark module and returns the module's string dict.
@@ -37,16 +38,21 @@ func NewTestLoader(moduleName string, loader ModuleLoadFunc) ThreadLoadFunc {
 }
 
 // ExecModuleTestScript executes a Starlark script with a module loader and returns the script's string dict.
-func ExecModuleTestScript(t *testing.T, name string, loader ModuleLoadFunc, script string) (starlark.StringDict, error) {
+func ExecModuleTestScript(t *testing.T, name string, loader ModuleLoadFunc, script string, wantErr error) (starlark.StringDict, error) {
 	thread := &starlark.Thread{Load: NewTestLoader(name, loader)}
 	starlarktest.SetReporter(thread, t)
 
 	out, err := starlark.ExecFile(thread, name+"_test.star", []byte(script), nil)
 	if err != nil {
-		if ee, ok := err.(*starlark.EvalError); ok {
-			t.Error(ee.Backtrace())
-		} else {
-			t.Error(err)
+		if wantErr == nil {
+			if ee, ok := err.(*starlark.EvalError); ok {
+				t.Errorf("got unexpected starlark error: '%v'", ee.Backtrace())
+			} else {
+				t.Errorf("got unexpected error: '%v'", err)
+			}
+		}
+		if wantErr != nil && err.Error() != wantErr.Error() {
+			t.Errorf("got mismatched error: '%v', want: '%v'", err, wantErr)
 		}
 	}
 	return out, err
