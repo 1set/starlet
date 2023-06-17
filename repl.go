6bd482a9c81f677ecf370f6b4ac1d99964c4b7ef
Hyori 효리 <hyorigo@icloud.com>
2023-06-18 00:43:40 +0800

    For REPL funcs

--- repl.go
+++ repl.go
@@ -5,6 +5,11 @@ import (
 )
 
 // REPL is a Read-Eval-Print-Loop for Starlark.
+// It loads the predeclared symbols and modules into the global environment,
 func (m *Machine) REPL() {
+	if err := m.prepareThread(nil); err != nil {
+		repl.PrintError(err)
+		return
+	}
 	repl.REPL(m.thread, m.predeclared)
 }
