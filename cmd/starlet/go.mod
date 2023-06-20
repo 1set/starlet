810233cc81f792531ad5481560d1d40f792b6cb4
Hyori 효리 <hyorigo@icloud.com>
2023-06-20 19:02:44 +0800

    For REPL

--- cmd/starlet/go.mod
+++ cmd/starlet/go.mod
@@ -6,11 +6,14 @@ require (
 	bitbucket.org/ai69/colorlogo v0.1.1
 	bitbucket.org/neiku/winornot v0.0.3
 	github.com/1set/gut v0.0.0-20201117175203-a82363231997
+	github.com/1set/starlet v0.0.0-20230617170942-d65f72a474f5
 	github.com/spf13/pflag v1.0.5
 )
 
 require (
+	github.com/1set/starlight v0.0.1 // indirect
 	github.com/aymanbagabas/go-osc52 v1.0.3 // indirect
+	github.com/chzyer/readline v0.0.0-20180603132655-2972be24d48e // indirect
 	github.com/gonutz/w32 v1.0.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.16 // indirect
@@ -18,5 +21,6 @@ require (
 	github.com/muesli/termenv v0.13.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/rodolfoag/gow32 v0.0.0-20160917004320-d95ff468acf8 // indirect
+	go.starlark.net v0.0.0-20230525235612-a134d8f9ddca // indirect
 	golang.org/x/sys v0.4.0 // indirect
 )
--- cmd/starlet/main.go
+++ cmd/starlet/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"fmt"
+	"io/fs"
 	"os"
 
 	"bitbucket.org/neiku/winornot"
 	"github.com/1set/gut/ystring"
+	"github.com/1set/starlet"
 	flag "github.com/spf13/pflag"
 )
 
@@ -18,10 +20,14 @@ var (
 	codeContent         string
 )
 
+var (
+	defaultPreloadModules = starlet.ListBuiltinModules()
+)
+
 func init() {
 	flag.BoolVarP(&allowRecursion, "recursion", "r", false, "allow recursion in Starlark code")
 	flag.BoolVarP(&allowGlobalReassign, "globalreassign", "g", false, "allow reassigning global variables in Starlark code")
-	flag.StringSliceVarP(&preloadModules, "preload", "p", nil, "preload modules before executing Starlark code")
+	flag.StringSliceVarP(&preloadModules, "preload", "p", defaultPreloadModules, "preload modules before executing Starlark code")
 	flag.StringSliceVarP(&lazyLoadModules, "lazyload", "l", nil, "lazy load modules when executing Starlark code")
 	flag.StringVarP(&includePath, "include", "i", ".", "include path for Starlark code to load modules from")
 	flag.StringVarP(&codeContent, "code", "c", "", "Starlark code to execute")
@@ -37,20 +43,37 @@ func main() {
 }
 
 func processArgs() int {
+	// get starlet machine
+	if allowRecursion {
+		starlet.EnableRecursionSupport()
+	}
+	if allowGlobalReassign {
+		starlet.EnableGlobalReassign()
+	}
+	mac := starlet.NewWithNames(nil, preloadModules, lazyLoadModules)
+	var incFS fs.FS
+	if ystring.IsNotBlank(includePath) {
+		incFS = os.DirFS(includePath)
+	}
+
+	// check arguments
 	nargs := flag.NArg()
 	hasCode := ystring.IsNotBlank(codeContent)
 	switch {
 	case nargs == 0 && hasCode:
-	// run code from command line
+		// run code from command line
 	case nargs == 0 && !hasCode:
 		// run REPL
+		mac.SetScript("repl", nil, incFS)
+		mac.REPL()
 	case nargs == 1:
 	// run code from file
 	case nargs > 1:
 		fmt.Println(`want at most one Starlark file name`)
+		return 1
 	default:
 		flag.Usage()
+		return 1
 	}
-
 	return 0
 }
