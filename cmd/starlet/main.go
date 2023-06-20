a1aa6e33ec24dfca9c35eceef7061d70652f107a
Hyori 효리 <hyorigo@icloud.com>
2023-06-20 15:59:52 +0800

    For CLI args

--- cmd/starlet/main.go
+++ cmd/starlet/main.go
@@ -1,22 +1,40 @@
 package main
 
 import (
+	"os"
+
 	"bitbucket.org/neiku/winornot"
 	flag "github.com/spf13/pflag"
 )
 
 var (
-	number int
+	allowRecursion      bool
+	allowGlobalReassign bool
+	preloadModules      []string
+	lazyLoadModules     []string
+	includePath         string
+	codeContent         string
 )
 
 func init() {
-	flag.IntVarP(&number, "number", "n", 1, "Example of number variable")
+	flag.BoolVarP(&allowRecursion, "recursion", "r", false, "allow recursion in Starlark code")
+	flag.BoolVarP(&allowGlobalReassign, "globalreassign", "g", false, "allow reassigning global variables in Starlark code")
+	flag.StringSliceVarP(&preloadModules, "preload", "p", nil, "preload modules before executing Starlark code")
+	flag.StringSliceVarP(&lazyLoadModules, "lazyload", "l", nil, "lazy load modules when executing Starlark code")
+	flag.StringVarP(&includePath, "include", "i", ".", "include path for Starlark code to load modules from")
+	flag.StringVarP(&codeContent, "code", "c", "", "Starlark code to execute")
 	flag.Parse()
 
 	// fix for Windows terminal output
 	winornot.EnableANSIControl()
+	displayBuildInfo() // TODO: move inside
 }
 
 func main() {
-	displayBuildInfo()
+	os.Exit(processArgs())
+}
+
+func processArgs() int {
+
+	return 0
 }
