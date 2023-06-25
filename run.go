12184a96b6fcb4dc8b988b274397166a30eb8786
Hyori 효리 <hyorigo@icloud.com>
2023-06-25 19:45:23 +0800

    Merged repl.go

--- run.go
+++ run.go
@@ -8,8 +8,19 @@ import (
 
 	"github.com/1set/starlet/lib/goidiomatic"
 	"github.com/1set/starlight/convert"
+	"go.starlark.net/repl"
 	"go.starlark.net/starlark"
 )
+
+// REPL is a Read-Eval-Print-Loop for Starlark.
+// It loads the predeclared symbols and modules into the global environment,
+func (m *Machine) REPL() {
+	if err := m.prepareThread(nil); err != nil {
+		repl.PrintError(err)
+		return
+	}
+	repl.REPL(m.thread, m.predeclared)
+}
 
 // RunScript initiates a Machine, executes a script with extra variables, and returns the Machine and the execution result.
 func RunScript(content []byte, extras StringAnyMap) (*Machine, StringAnyMap, error) {
