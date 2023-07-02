4ce5f6a4b28ab8a006dc0f40cefd047e53474ca9
Hyori 효리 <hyorigo@icloud.com>
2023-07-02 22:14:49 +0800

    Migrate

---
+++ lib/re/re_test.go
@@ -0,0 +1,20 @@
+package re
+
+import (
+	"testing"
+
+	"github.com/qri-io/starlib/testdata"
+	"go.starlark.net/starlark"
+	"go.starlark.net/starlarktest"
+)
+
+func TestFile(t *testing.T) {
+	thread := &starlark.Thread{Load: testdata.NewLoader(LoadModule, ModuleName)}
+	starlarktest.SetReporter(thread, t)
+
+	// Execute test file
+	_, err := starlark.ExecFile(thread, "testdata/test.star", nil, nil)
+	if err != nil {
+		t.Error(err)
+	}
+}
