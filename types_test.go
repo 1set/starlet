a68f1504bafe901376a0692a28fd4bd4d84cb18a
Hyori 효리 <hyorigo@icloud.com>
2023-08-30 15:25:25 +0800

    For test string any

---
+++ types_test.go
@@ -0,0 +1,112 @@
+package starlet
+
+import (
+	"reflect"
+	"testing"
+
+	"go.starlark.net/starlark"
+)
+
+func TestStringAnyMap_Clone(t *testing.T) {
+	tests := []struct {
+		name string
+		d    StringAnyMap
+		want StringAnyMap
+	}{
+		{
+			name: "test_empty_map_clone",
+			d:    make(StringAnyMap),
+			want: make(StringAnyMap),
+		},
+		{
+			name: "test_non_empty_map_clone",
+			d:    StringAnyMap{"key1": "val1", "key2": 2, "key3": true},
+			want: StringAnyMap{"key1": "val1", "key2": 2, "key3": true},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := tt.d.Clone(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Clone() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestStringAnyMap_Merge(t *testing.T) {
+	tests := []struct {
+		name       string
+		d          StringAnyMap
+		other      StringAnyMap
+		want       StringAnyMap
+		wantEffect bool
+	}{
+		{
+			name:       "test_nil_merge",
+			d:          nil,
+			other:      StringAnyMap{"key1": "val1"},
+			wantEffect: false,
+		},
+		{
+			name:       "test_merge_map",
+			d:          StringAnyMap{"key1": "val1", "key2": 2},
+			other:      StringAnyMap{"key2": "val2", "key3": true},
+			want:       StringAnyMap{"key1": "val1", "key2": "val2", "key3": true},
+			wantEffect: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.d != nil {
+				orig := tt.d.Clone()
+				tt.d.Merge(tt.other)
+				if tt.wantEffect && !reflect.DeepEqual(tt.d, tt.want) {
+					t.Errorf("Merge() got = %v, want %v", tt.d, tt.want)
+				} else if !tt.wantEffect && !reflect.DeepEqual(tt.d, orig) {
+					t.Errorf("Merge() got = %v, want original map: %v", tt.d, orig)
+				}
+			}
+		})
+	}
+}
+
+// Assuming starlark.StringDict is equivalent to map[string]interface{} for this example.
+func TestStringAnyMap_MergeDict(t *testing.T) {
+	tests := []struct {
+		name       string
+		d          StringAnyMap
+		other      starlark.StringDict
+		want       StringAnyMap
+		wantEffect bool
+	}{
+		{
+			name:       "test_nil_merge_dict",
+			d:          nil,
+			other:      starlark.StringDict{"key1": starlark.String("val1")},
+			wantEffect: false,
+		},
+		{
+			name:       "test_merge_dict",
+			d:          StringAnyMap{"key1": "val1", "key2": 2},
+			other:      starlark.StringDict{"key2": starlark.String("val2"), "key3": starlark.Bool(true)},
+			want:       StringAnyMap{"key1": "val1", "key2": starlark.String("val2"), "key3": starlark.Bool(true)},
+			wantEffect: true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.d != nil {
+				orig := tt.d.Clone()
+				tt.d.MergeDict(tt.other)
+				if tt.wantEffect && !reflect.DeepEqual(tt.d, tt.want) {
+					t.Errorf("MergeDict() got = %v, want %v", tt.d, tt.want)
+				} else if !tt.wantEffect && !reflect.DeepEqual(tt.d, orig) {
+					t.Errorf("MergeDict() got = %v, want original map: %v", tt.d, orig)
+				}
+			}
+		})
+	}
+}
