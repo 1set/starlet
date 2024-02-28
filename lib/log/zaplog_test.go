34732e738df2d56167526eeb64f57dab2456f70e
Hyori 효리 <hyorigo@icloud.com>
2024-02-28 17:43:39 +0800

    for testing

--- lib/log/zaplog_test.go
+++ lib/log/zaplog_test.go
@@ -1,17 +1,23 @@
 package log_test
 
 import (
+	"bytes"
+	"fmt"
+	"strings"
 	"testing"
 
 	itn "github.com/1set/starlet/internal"
 	lg "github.com/1set/starlet/lib/log"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
 )
 
 func TestLoadModule_Log(t *testing.T) {
 	tests := []struct {
-		name    string
-		script  string
-		wantErr string
+		name     string
+		script   string
+		wantErr  string
+		keywords []string
 	}{
 		{
 			name: `debug message`,
@@ -19,6 +25,7 @@ func TestLoadModule_Log(t *testing.T) {
 				load('log', 'debug')
 				debug('this is a debug message only')
 			`),
+			keywords: []string{"DEBUG", "this is a debug message only"},
 		},
 		{
 			name: `debug with no args`,
@@ -37,6 +44,14 @@ func TestLoadModule_Log(t *testing.T) {
 			wantErr: "log.debug: expected string as first argument, got int",
 		},
 		{
+			name: `debug with incomplete args`,
+			script: itn.HereDoc(`
+				load('log', 'debug')
+				debug('this is a broken message', "what")
+			`),
+			keywords: []string{"DEBUG", "this is a broken message", "ERROR", "Ignored key without a value."},
+		},
+		{
 			name: `debug with key values`,
 			script: itn.HereDoc(`
 				load('log', 'debug')
@@ -48,11 +63,54 @@ func TestLoadModule_Log(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			l, b := buildCustomLogger()
+			lg.SetLog(l)
 			res, err := itn.ExecModuleWithErrorTest(t, lg.ModuleName, lg.LoadModule, tt.script, tt.wantErr)
 			if (err != nil) != (tt.wantErr != "") {
 				t.Errorf("log(%q) expects error = '%v', actual error = '%v', result = %v", tt.name, tt.wantErr, err, res)
 				return
 			}
+			if len(tt.wantErr) > 0 {
+				return
+			}
+			if len(tt.keywords) > 0 {
+				bs := b.String()
+				for _, k := range tt.keywords {
+					if !strings.Contains(bs, k) {
+						t.Errorf("log(%q) expects keyword = '%v', actual log = '%v'", tt.name, k, bs)
+						return
+					}
+				}
+			} else {
+				fmt.Println(b.String())
+			}
 		})
 	}
+}
+
+func buildCustomLogger() (*zap.SugaredLogger, *bytes.Buffer) {
+	buf := bytes.NewBufferString("")
+	var al zap.LevelEnablerFunc = func(lvl zapcore.Level) bool {
+		return true
+	}
+	ce := zapcore.NewConsoleEncoder(zapcore.EncoderConfig{
+		TimeKey:        "time",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		CallerKey:      "caller",
+		MessageKey:     "msg",
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.CapitalLevelEncoder,
+		EncodeTime:     zapcore.ISO8601TimeEncoder,
+		EncodeDuration: zapcore.StringDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder,
+	})
+	cr := zapcore.NewCore(ce, zapcore.AddSync(buf), al)
+	op := []zap.Option{
+		zap.AddCaller(),
+		//zap.AddStacktrace(zap.ErrorLevel),
+	}
+	logger := zap.New(cr, op...)
+	return logger.Sugar(), buf
 }
