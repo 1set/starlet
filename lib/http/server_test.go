ec6f21d0016bfdd741fe70196597d5671885f8d7
Hyori 효리 <hyorigo@icloud.com>
2024-03-18 17:06:01 +0800

    fix test checks

--- lib/http/server_test.go
+++ lib/http/server_test.go
@@ -35,7 +35,8 @@ func TestConvertServerRequest(t *testing.T) {
 		t.Error("ConvertServerRequest returned nil")
 		return
 	}
-	// Request(body = "{\"name\":\"John\",\"age\":30}", encoding = [], header = {"Content-Type": ["application/json"], "X-Custom-Header": ["Custom Value 1", "Custom Value 2"]}, host = "", json = {"name": "John", "age": 30}, method = "POST", proto = "HTTP/1.1", query = {"param1": ["value1"], "param2": ["value2", "value_two"], "ver": ["0317"]}, remote = "127.0.0.1:12345", url = "/?param1=value1&param2=value2&param2=value_two&ver=0317")
+
+	// prepare expected values
 	sd := starlark.NewDict(2)
 	_ = sd.SetKey(starlark.String("name"), starlark.String("John"))
 	_ = sd.SetKey(starlark.String("age"), starlark.MakeInt(30))
@@ -51,23 +52,30 @@ func TestConvertServerRequest(t *testing.T) {
 		name string
 		want starlark.Value
 	}{
-		{"body", starlark.String(jsonBody)},
-		{"json", sd},
 		{"host", starlark.String("")},
 		{"remote", starlark.String("127.0.0.1:12345")},
 		{"proto", starlark.String("HTTP/1.1")},
 		{"method", starlark.String("POST")},
+		{"body", starlark.String(jsonBody)},
+		{"json", sd},
 		{"header", sh},
 		{"query", sq},
 	}
+
+	// check the fields
 	for _, f := range fields {
 		got, err := sr.Attr(f.name)
 		if err != nil {
 			t.Errorf("fail to get Request(%s) from struct: %v", f.name, err)
 			return
 		}
-		if !reflect.DeepEqual(got, f.want) {
-			t.Errorf("Request(%s) got = %v, want %v", f.name, got, f.want)
+		if yes, err := starlark.Equal(got, f.want); err == nil {
+			if !yes {
+				t.Errorf("Request(%s)[SL] got = %v, want %v", f.name, got, f.want)
+				return
+			}
+		} else if !reflect.DeepEqual(got, f.want) {
+			t.Errorf("Request(%s)[GO] got = %v, want %v", f.name, got, f.want)
 			return
 		}
 	}
