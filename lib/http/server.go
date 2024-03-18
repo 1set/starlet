b5ea6af9e604dc156c327305f4b86404d89f3cf1
Hyori 효리 <hyorigo@icloud.com>
2024-03-18 15:41:56 +0800

    for headers

--- lib/http/server.go
+++ lib/http/server.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 
 	"github.com/1set/starlet/dataconv"
+	itn "github.com/1set/starlet/internal"
 	"go.starlark.net/starlark"
 	"go.starlark.net/starlarkstruct"
 )
@@ -70,4 +71,65 @@ func sliceStr2List(s []string) *starlark.List {
 		l[i] = starlark.String(v)
 	}
 	return starlark.NewList(l)
+}
+
+type contentDataType uint
+
+const (
+	contentDataBinary contentDataType = iota
+	contentDataJSON
+	contentDataText
+	contentDataHTML
+)
+
+// ServerResponse is a Starlark struct to save info in Starlark scripts to modify http.ResponseWriter outside.
+type ServerResponse struct {
+	statusCode  int
+	headers     map[string][]string
+	contentType string
+	dataType    contentDataType
+	data        []byte
+}
+
+func (r *ServerResponse) setStatus(thread *starlark.Thread, b *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+	var code uint8
+	if err := starlark.UnpackPositionalArgs(b.Name(), args, nil, 1, &code); err != nil {
+		return nil, err
+	}
+	r.statusCode = int(code)
+	return starlark.None, nil
+}
+
+func (r *ServerResponse) setData(dt contentDataType) func(thread *starlark.Thread, b *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+	return func(thread *starlark.Thread, b *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+		var data itn.StringOrBytes
+		if err := starlark.UnpackPositionalArgs(b.Name(), args, nil, 1, &data); err != nil {
+			return nil, err
+		}
+		r.dataType = dt
+		r.data = data.GoBytes()
+		return starlark.None, nil
+	}
+}
+
+func (r *ServerResponse) setContentType(thread *starlark.Thread, b *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+	var ct itn.StringOrBytes
+	if err := starlark.UnpackPositionalArgs(b.Name(), args, nil, 1, &ct); err != nil {
+		return nil, err
+	}
+	r.contentType = ct.GoString()
+	return starlark.None, nil
+}
+
+func (r *ServerResponse) setHeader(thread *starlark.Thread, b *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+	var key, value itn.StringOrBytes
+	if err := starlark.UnpackArgs(b.Name(), args, kwargs, "key", &key, "value", &value); err != nil {
+		return nil, err
+	}
+	k, v := key.GoString(), value.GoString()
+	if r.headers == nil {
+		r.headers = make(map[string][]string)
+	}
+	r.headers[k] = append(r.headers[k], v)
+	return starlark.None, nil
 }
