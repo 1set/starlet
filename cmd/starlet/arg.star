1790e9657875be022265a21928df1ac6c2fe4642
Hyori 효리 <hyorigo@icloud.com>
2024-03-19 11:35:25 +0800

    another file

---
+++ cmd/starlet/arg.star
@@ -0,0 +1,5 @@
+import sys
+
+print("cnt:", len(sys.argv))
+print("argv:", sys.argv)
+print("platform:", sys.platform)
--- cmd/starlet/main.go
+++ cmd/starlet/main.go
@@ -4,14 +4,11 @@ package main
 import (
 	"fmt"
 	"io/fs"
-	"log"
-	"net/http"
 	"os"
 
 	"bitbucket.org/neiku/winornot"
 	"github.com/1set/gut/ystring"
 	"github.com/1set/starlet"
-	shttp "github.com/1set/starlet/lib/http"
 	flag "github.com/spf13/pflag"
 	"go.starlark.net/starlark"
 	"golang.org/x/term"
@@ -129,78 +126,6 @@ func processArgs() int {
 		return 1
 	}
 	return 0
-}
-
-func runWebServer(port uint16, setCode func(m *starlet.Machine)) error {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// prepare envs
-		resp := shttp.NewServerResponse()
-		glb := starlet.StringAnyMap{
-			"request":  shttp.ConvertServerRequest(r),
-			"response": resp.Struct(),
-		}
-
-		// run code
-		mac := starlet.NewWithNames(glb, preloadModules, lazyLoadModules)
-		setCode(mac)
-		_, err := mac.Run()
-
-		// handle error
-		if err != nil {
-			log.Printf("Runtime Error: %v\n", err)
-			w.Header().Set("Content-Type", "text/plain")
-			w.WriteHeader(http.StatusInternalServerError)
-			if _, err := fmt.Fprintf(w, "Runtime Error: %v", err); err != nil {
-				log.Printf("Error writing response: %v", err)
-			}
-			return
-		}
-
-		// handle response
-		if err = resp.Write(w); err != nil {
-			w.Header().Add("Content-Type", "text/plain")
-			w.WriteHeader(http.StatusInternalServerError)
-			_, _ = w.Write([]byte(err.Error()))
-		}
-	})
-
-	log.Printf("Server is starting on port: %d\n", port)
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), mux)
-	if err != nil {
-		log.Fatalf("Server failed to start: %v", err)
-	}
-	return err
-}
-
-func runWebServerLegacy(port uint16, setCode func(m *starlet.Machine)) error {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		glb := starlet.StringAnyMap{
-			"reader":  r,
-			"writer":  w,
-			"fprintf": fmt.Fprintf,
-		}
-
-		mac := starlet.NewWithNames(glb, preloadModules, lazyLoadModules)
-		setCode(mac)
-		//mac.SetScript("web.star", code, incFS)
-		if _, err := mac.Run(); err != nil {
-			log.Printf("Runtime Error: %v\n", err)
-			w.WriteHeader(http.StatusInternalServerError)
-			if _, err := fmt.Fprintf(w, "Runtime Error: %v", err); err != nil {
-				log.Printf("Error writing response: %v", err)
-			}
-			return
-		}
-	})
-
-	log.Printf("Server is starting on port: %d\n", port)
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), mux)
-	if err != nil {
-		log.Fatalf("Server failed to start: %v", err)
-	}
-	return err
 }
 
 // PrintError prints the error to stderr,
