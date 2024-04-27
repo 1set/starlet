381aed6a796306f999a0e5e5300cd57055890e45
Hyori 효리 <hyorigo@icloud.com>
2024-04-27 23:09:17 +0800

    for emptyStr

--- lib/file/copy.go
+++ lib/file/copy.go
@@ -32,28 +32,28 @@ func copyFile(thread *starlark.Thread, b *starlark.Builtin, args starlark.Tuple,
 // Symbolic links are followed on both source and destination.
 func copyFileGo(src, dst string, overwrite bool) (string, error) {
 	// No empty input
-	if src == "" {
-		return "", errors.New("source file is empty")
+	if src == emptyStr {
+		return emptyStr, errors.New("source file is empty")
 	}
-	if dst == "" {
-		return "", errors.New("destination file is empty")
+	if dst == emptyStr {
+		return emptyStr, errors.New("destination file is empty")
 	}
 
 	// Open the source file.
 	srcFile, err := os.Open(src)
 	if err != nil {
-		return "", fmt.Errorf("open source file: %w", err)
+		return emptyStr, fmt.Errorf("open source file: %w", err)
 	}
 	defer srcFile.Close()
 
 	// Stat the source file to get its mode, times, and owner.
 	srcStat, err := srcFile.Stat()
 	if err != nil {
-		return "", fmt.Errorf("stat source file: %w", err)
+		return emptyStr, fmt.Errorf("stat source file: %w", err)
 	}
 	if !srcStat.Mode().IsRegular() {
 		// HACK, not sure if this is the best way to check if the file is a regular file
-		return "", errors.New("source file is not a regular file")
+		return emptyStr, errors.New("source file is not a regular file")
 	}
 
 	// Check if dst is a directory, and adjust the destination path if it is
@@ -67,39 +67,39 @@ func copyFileGo(src, dst string, overwrite bool) (string, error) {
 	}
 	if err != nil && !os.IsNotExist(err) {
 		// for errors other than file not exists
-		return "", err
+		return emptyStr, err
 	}
 
 	// for destination file exists
 	if err == nil {
 		// If the source and destination files are the same, return an error.
 		if os.SameFile(srcStat, dstStat) {
-			return "", fmt.Errorf("source and destination are the same file: %s", src)
+			return emptyStr, fmt.Errorf("source and destination are the same file: %s", src)
 		}
 		// If overwrite is false, return an error if the destination file exists.
 		if !overwrite {
-			return "", &os.PathError{Op: "copy", Path: dst, Err: os.ErrExist}
+			return emptyStr, &os.PathError{Op: "copy", Path: dst, Err: os.ErrExist}
 		}
 	}
 
 	// Create the destination file.
 	dstFile, err := os.Create(dst)
 	if err != nil {
-		return "", fmt.Errorf("cannot create file: %w", err)
+		return emptyStr, fmt.Errorf("cannot create file: %w", err)
 	}
 	defer dstFile.Close()
 
 	// Copy the source file to the destination file.
 	if _, err := io.Copy(dstFile, srcFile); err != nil {
-		return "", fmt.Errorf("cannot copy file: %w", err)
+		return emptyStr, fmt.Errorf("cannot copy file: %w", err)
 	}
 
 	// Set the mode, times file to match the source file. HACK: ignore the error maybe
 	if err := os.Chmod(dst, srcStat.Mode()); err != nil {
-		return "", fmt.Errorf("cannot chmod file: %w", err)
+		return emptyStr, fmt.Errorf("cannot chmod file: %w", err)
 	}
 	if err := os.Chtimes(dst, srcStat.ModTime(), srcStat.ModTime()); err != nil {
-		return "", fmt.Errorf("cannot chtimes file: %w", err)
+		return emptyStr, fmt.Errorf("cannot chtimes file: %w", err)
 	}
 	return dst, nil
 }
