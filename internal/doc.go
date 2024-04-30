0a66267529a74a27d33c2bfabbe4d8168e738c5e
Hyori 효리 <hyorigo@icloud.com>
2024-04-30 19:16:16 +0800

    update versions

--- internal/doc.go
+++ internal/doc.go
@@ -2,5 +2,5 @@
 // It should be only imported by the custom Starlark modules under starlet/lib folders, and not by the Starlet main package to avoid cyclic import.
 package internal
 
-// StarletVersion is the current version of Starlet.
-const StarletVersion = "v0.0.6"
+// StarletVersion should be the current version of Starlet.
+const StarletVersion = "v0.1.0"
