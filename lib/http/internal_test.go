4f35d5323074f058918ebd5366c47297672a2520
Hyori 효리 <hyorigo@icloud.com>
2024-04-30 16:17:25 +0800

    another test

---
+++ lib/http/internal_test.go
@@ -0,0 +1,73 @@
+package http
+
+import (
+	"io/ioutil"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"go.starlark.net/starlark"
+)
+
+// we're ok with testing private functions if it simplifies the test :)
+func TestSetBody(t *testing.T) {
+	fd := map[string]string{
+		"foo": "bar baz",
+	}
+
+	cases := []struct {
+		rawBody      starlark.String
+		formData     map[string]string
+		formEncoding starlark.String
+		jsonData     starlark.Value
+		body         string
+		err          string
+	}{
+		{starlark.String("hallo"), nil, starlark.String(""), nil, "hallo", ""},
+		{starlark.String(""), fd, starlark.String(""), nil, "foo=bar+baz", ""},
+		// TODO - this should check multipart form data is being set
+		{starlark.String(""), fd, starlark.String("multipart/form-data"), nil, "", ""},
+		{starlark.String(""), nil, starlark.String(""), starlark.Tuple{starlark.Bool(true), starlark.MakeInt(1), starlark.String("der")}, "[true,1,\"der\"]", ""},
+	}
+
+	for i, c := range cases {
+		var formData *starlark.Dict
+		if c.formData != nil {
+			formData = starlark.NewDict(len(c.formData))
+			for k, v := range c.formData {
+				if err := formData.SetKey(starlark.String(k), starlark.String(v)); err != nil {
+					t.Fatal(err)
+				}
+			}
+		}
+
+		req := httptest.NewRequest("get", "https://example.com", nil)
+		err := setBody(req, c.rawBody, formData, c.formEncoding, c.jsonData)
+		if !(err == nil && c.err == "" || (err != nil && err.Error() == c.err)) {
+			t.Errorf("case %d error mismatch. expected: %s, got: %s", i, c.err, err)
+			continue
+		}
+
+		if strings.HasPrefix(req.Header.Get("Content-Type"), "multipart/form-data;") {
+			if err := req.ParseMultipartForm(0); err != nil {
+				t.Fatal(err)
+			}
+
+			for k, v := range c.formData {
+				fv := req.FormValue(k)
+				if fv != v {
+					t.Errorf("case %d error mismatch. expected %s=%s, got: %s", i, k, v, fv)
+				}
+			}
+		} else {
+			body, err := ioutil.ReadAll(req.Body)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			if string(body) != c.body {
+				t.Errorf("case %d body mismatch. expected: %s, got: %s", i, c.body, string(body))
+			}
+		}
+	}
+}
