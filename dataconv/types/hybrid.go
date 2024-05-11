390d66a6fabea37314555be8b011b132d91cc78a
Hyori 효리 <hyorigo@icloud.com>
2024-05-11 14:30:34 +0800

    split files

--- dataconv/types/hybrid.go
+++ dataconv/types/hybrid.go
@@ -6,19 +6,6 @@ import (
 	"go.starlark.net/starlark"
 )
 
-var (
-	emptyStr string
-)
-
-// Unpacker is an interface for types that can be unpacked from Starlark values.
-var (
-	_ starlark.Unpacker = (*FloatOrInt)(nil)
-	_ starlark.Unpacker = (*StringOrBytes)(nil)
-	_ starlark.Unpacker = (*NullableString)(nil)
-	_ starlark.Unpacker = (*NullableDict)(nil)
-	_ starlark.Unpacker = (*NumericValue)(nil)
-)
-
 // FloatOrInt is an Unpacker that converts a Starlark int or float to Go's float64.
 type FloatOrInt float64
 
@@ -75,77 +62,6 @@ func (p StringOrBytes) GoString() string {
 // StarlarkString returns the Starlark string representation of the StringOrBytes.
 func (p StringOrBytes) StarlarkString() starlark.String {
 	return starlark.String(p)
-}
-
-// NullableString is an Unpacker that converts a Starlark None or string to Go's string.
-type NullableString struct {
-	str *string
-}
-
-// NewNullableString creates and returns a new NullableString.
-func NewNullableString(s string) *NullableString {
-	return &NullableString{str: &s}
-}
-
-// Unpack implements Unpacker.
-func (p *NullableString) Unpack(v starlark.Value) error {
-	switch v := v.(type) {
-	case starlark.String:
-		s := string(v)
-		p.str = &s
-	case starlark.Bytes:
-		s := string(v)
-		p.str = &s
-	case starlark.NoneType:
-		p.str = nil
-	default:
-		return fmt.Errorf("got %s, want string, bytes or None", v.Type())
-	}
-	return nil
-}
-
-// GoString returns the Go string representation of the NullableString, if the underlying value is nil, it returns an empty string.
-func (p *NullableString) GoString() string {
-	if p == nil || p.str == nil {
-		return ""
-	}
-	return *p.str
-}
-
-// IsNull returns true if the underlying value is nil.
-func (p *NullableString) IsNull() bool {
-	return p == nil || p.str == nil
-}
-
-// IsNullOrEmpty returns true if the underlying value is nil or an empty string.
-func (p *NullableString) IsNullOrEmpty() bool {
-	return p.IsNull() || p.GoString() == emptyStr
-}
-
-// NullableDict is an Unpacker that converts a Starlark None or Dict to Go's *starlark.Dict.
-type NullableDict struct {
-	dict *starlark.Dict
-}
-
-// Unpack implements Unpacker.
-func (p *NullableDict) Unpack(v starlark.Value) error {
-	switch v := v.(type) {
-	case *starlark.Dict:
-		p.dict = v
-	case starlark.NoneType:
-		p.dict = nil
-	default:
-		return fmt.Errorf("got %s, want dict or None", v.Type())
-	}
-	return nil
-}
-
-// AsDict returns the *starlark.Dict representation of the NullableDict, if the underlying dict is nil, it returns an new empty dict.
-func (p *NullableDict) AsDict() *starlark.Dict {
-	if p.dict == nil {
-		return starlark.NewDict(0)
-	}
-	return p.dict
 }
 
 // NumericValue holds a Starlark numeric value and tracks its type.
