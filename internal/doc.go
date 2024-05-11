7ecf2e27d21670255b6f5df9b50c0741f843ed54
Hyori 효리 <hyorigo@icloud.com>
2024-05-11 14:37:22 +0800

    less one

--- internal/doc.go
+++ internal/doc.go
@@ -2,5 +2,15 @@
 // It should be only imported by the custom Starlark modules under starlet/lib folders, and not by the Starlet main package to avoid cyclic import.
 package internal
 
+import "github.com/h2so5/here"
+
 // StarletVersion should be the current version of Starlet.
 const StarletVersion = "v0.1.0"
+
+var (
+	// HereDoc returns un-indented string as here-document.
+	HereDoc = here.Doc
+
+	// HereDocf returns unindented and formatted string as here-document. Formatting is done as for fmt.Printf().
+	HereDocf = here.Docf
+)
