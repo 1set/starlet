96ab68c011ac8395a91898f0c6fd1b08cb2e43c0
Hyori 효리 <hyorigo@icloud.com>
2024-05-11 17:19:04 +0800

    later 2

--- dataconv/types/nullable.go
+++ dataconv/types/nullable.go
@@ -10,7 +10,6 @@ import (
 var (
 	_ starlark.Unpacker = (*NullableString)(nil)
 	_ starlark.Unpacker = (*NullableDict)(nil)
-	_ starlark.Unpacker = (*NullableInt)(nil)
 )
 
 // NullableString is an Unpacker that converts a Starlark None or string to Go's string.
@@ -83,45 +82,3 @@ func (p *NullableDict) AsDict() *starlark.Dict {
 	}
 	return p.dict
 }
-
-// Nullable is an Unpacker that converts a Starlark None or T to Go's starlark.Value.
-type Nullable[T starlark.Value] struct {
-	value        *T
-	defaultValue T
-}
-
-// NewNullable creates and returns a new Nullable.
-func NewNullable[T starlark.Value](defaultValue T) *Nullable[T] {
-	return &Nullable[T]{value: nil, defaultValue: defaultValue}
-}
-
-// Unpack implements Unpacker.
-func (p *Nullable[T]) Unpack(v starlark.Value) error {
-	if p == nil {
-		return fmt.Errorf("nil pointer")
-	}
-	if _, ok := v.(starlark.NoneType); ok {
-		p.value = nil
-	} else if t, ok := v.(T); ok {
-		p.value = &t
-	} else {
-		return fmt.Errorf("got %s, want %T or None", v.Type(), p.defaultValue.Type())
-	}
-	return nil
-}
-
-// IsNull returns true if the underlying value is nil.
-func (p *Nullable[T]) IsNull() bool {
-	return p == nil || p.value == nil
-}
-
-// Value returns the underlying value or default value if the underlying value is nil.
-func (p *Nullable[T]) Value() T {
-	if p.IsNull() {
-		return p.defaultValue
-	}
-	return *p.value
-}
-
-// NullableInt is an Unpacker that converts a Starlark None or Int.
-type NullableInt = Nullable[starlark.Int]
