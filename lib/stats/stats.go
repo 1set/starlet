f91da02c170b60f9fbc1205916f9e43084d988c8
Hyori 효리 <hyorigo@icloud.com>
2024-07-11 00:21:11 +0800

    rename val

--- lib/stats/stats.go
+++ lib/stats/stats.go
@@ -121,13 +121,13 @@ func newBinaryFloatBuiltin(name string, fn func(gms.Float64Data, gms.Float64Data
 func newBinaryFloatSingleBuiltin(name string, fn func(gms.Float64Data, float64) (float64, error)) *starlark.Builtin {
 	return starlark.NewBuiltin(name, func(thread *starlark.Thread, b *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
 		var (
-			data1 tps.FloatOrIntList
-			data2 tps.FloatOrInt
+			data tps.FloatOrIntList
+			val  tps.FloatOrInt
 		)
-		if err := starlark.UnpackPositionalArgs(name, args, kwargs, 2, &data1, &data2); err != nil {
+		if err := starlark.UnpackPositionalArgs(name, args, kwargs, 2, &data, &val); err != nil {
 			return nil, err
 		}
-		result, err := fn(data1.GoSlice(), data2.GoFloat())
+		result, err := fn(data.GoSlice(), val.GoFloat())
 		if err != nil {
 			return nil, err
 		}
