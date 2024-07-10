1af91f86f1a4f7b29df7b98b523bb483908e58bf
Hyori 효리 <hyorigo@icloud.com>
2024-07-10 20:24:09 +0800

    for modules

--- lib/stats/stats.go
+++ lib/stats/stats.go
@@ -2,13 +2,6 @@
 package stats
 
 import (
-	"crypto/md5"
-	"crypto/sha1"
-	"crypto/sha256"
-	"crypto/sha512"
-	"encoding/hex"
-	"hash"
-	"io"
 	"sync"
 
 	tps "github.com/1set/starlet/dataconv/types"
@@ -22,44 +15,20 @@ const ModuleName = "stats"
 
 var (
 	once       sync.Once
-	hashModule starlark.StringDict
-	hashError  error
+	statModule starlark.StringDict
 )
 
 // LoadModule loads the hashlib module. It is concurrency-safe and idempotent.
 func LoadModule() (starlark.StringDict, error) {
 	once.Do(func() {
-		hashModule = starlark.StringDict{
+		statModule = starlark.StringDict{
 			ModuleName: &starlarkstruct.Module{
-				Name: ModuleName,
-				Members: starlark.StringDict{
-					"md5":    starlark.NewBuiltin("hash.md5", fnHash(md5.New)),
-					"sha1":   starlark.NewBuiltin("hash.sha1", fnHash(sha1.New)),
-					"sha256": starlark.NewBuiltin("hash.sha256", fnHash(sha256.New)),
-					"sha512": starlark.NewBuiltin("hash.sha512", fnHash(sha512.New)),
-				},
+				Name:    ModuleName,
+				Members: starlark.StringDict{},
 			},
 		}
 	})
-	return hashModule, hashError
-}
-
-func fnHash(algo func() hash.Hash) func(*starlark.Thread, *starlark.Builtin, starlark.Tuple, []starlark.Tuple) (starlark.Value, error) {
-	return func(t *starlark.Thread, fn *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
-		// check args
-		var sb tps.StringOrBytes
-		if err := starlark.UnpackArgs(fn.Name(), args, kwargs, "data", &sb); err != nil {
-			return starlark.None, err
-		}
-
-		// get hash
-		h := algo()
-		_, err := io.WriteString(h, sb.GoString())
-		if err != nil {
-			return starlark.None, err
-		}
-		return starlark.String(hex.EncodeToString(h.Sum(nil))), nil
-	}
+	return statModule, nil
 }
 
 // newUnaryFloatBuiltin wraps a unary function accepting []float64 and returning (float64, error) as a Starlark built-in.
