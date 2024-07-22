2683c990cf78eaccb9ab7310cb42269487f746bb
Hyori 효리 <hyorigo@icloud.com>
2024-07-23 00:58:43 +0800

    add tests

--- lib/net/ping_test.go
+++ lib/net/ping_test.go
@@ -16,6 +16,7 @@ func TestLoadModule_Ping(t *testing.T) {
 		wantErr     string
 		skipWindows bool
 	}{
+		// TCPing tests
 		{
 			name: `tcping: normal`,
 			script: itn.HereDoc(`
@@ -78,6 +79,71 @@ func TestLoadModule_Ping(t *testing.T) {
 				tcping(123)
 			`),
 			wantErr: `net.tcping: for parameter hostname: got int, want string or bytes`,
+		},
+
+		// HTTPing tests
+		{
+			name: `httping: normal`,
+			script: itn.HereDoc(`
+				load('net', 'httping')
+				s = httping('https://www.bing.com')
+				print(s)
+				assert.eq(s.total, 4)
+				assert.true(s.success > 0)
+			`),
+		},
+		{
+			name: `httping: abnormal`,
+			script: itn.HereDoc(`
+				load('net', 'httping')
+				s = httping('https://www.bing.com', count=1, timeout=-5, interval=-2)
+				print(s)
+				assert.eq(s.total, 1)
+				assert.true(s.success > 0)
+				assert.eq(s.stddev, 0)
+			`),
+		},
+		{
+			name: `httping: faster`,
+			script: itn.HereDoc(`
+				load('net', 'httping')
+				s = httping('https://www.bing.com', count=10, timeout=5, interval=0.01)
+				print(s)
+				assert.eq(s.total, 10)
+				assert.true(s.success > 0)
+			`),
+		},
+		{
+			name: `httping: not exists`,
+			script: itn.HereDoc(`
+				load('net', 'httping')
+				s = httping('http://missing.invalid')
+			`),
+			wantErr: `net.httping: no successful connections`, // mac/win: no such host, linux: server misbehaving
+		},
+		{
+			name: `httping: wrong count`,
+			script: itn.HereDoc(`
+				load('net', 'httping')
+				s = httping('https://www.bing.com', count=0)
+			`),
+			wantErr: `net.httping: count must be greater than 0`,
+		},
+		{
+			name: `httping: no args`,
+			script: itn.HereDoc(`
+				load('net', 'httping')
+				httping()
+			`),
+			wantErr: `net.httping: missing argument for url`,
+		},
+		{
+			name: `httping: invalid args`,
+			script: itn.HereDoc(`
+				load('net', 'httping')
+				httping(123)
+			`),
+			wantErr: `net.httping: for parameter url: got int, want string or bytes`,
 		},
 	}
 	for _, tt := range tests {
