6463cf7e33f5d460d6b66a6303b741e8434d93f8
Hyori 효리 <hyorigo@icloud.com>
2024-11-12 07:02:59 +0800

    works now

--- lib/json/json.go
+++ lib/json/json.go
@@ -25,7 +25,9 @@ func LoadModule() (starlark.StringDict, error) {
 		mod := starlarkstruct.Module{
 			Name: ModuleName,
 			Members: starlark.StringDict{
-				"dumps": starlark.NewBuiltin("json.dumps", dumps),
+				"dumps":      starlark.NewBuiltin("json.dumps", dumps),
+				"try_encode": starlark.NewBuiltin("json.try_encode", tryEncode),
+				"try_decode": starlark.NewBuiltin("json.try_decode", tryDecode),
 			},
 		}
 		for k, v := range stdjson.Module.Members {
@@ -59,4 +61,51 @@ func dumps(thread *starlark.Thread, fn *starlark.Builtin, args starlark.Tuple, k
 		return starlark.None, err
 	}
 	return starlark.String(data), nil
+}
+
+func tryDumps(thread *starlark.Thread, fn *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+	var (
+		obj    starlark.Value
+		indent = starlark.MakeInt(0)
+	)
+	if err := starlark.UnpackArgs(fn.Name(), args, kwargs, "obj", &obj, "indent?", &indent); err != nil {
+		return starlark.Tuple{starlark.None, starlark.String(err.Error())}, nil
+	}
+
+	it, ok := indent.Int64()
+	if !ok || it < 0 {
+		it = 0
+	}
+
+	data, err := itn.MarshalStarlarkJSON(obj, int(it))
+	if err != nil {
+		return starlark.Tuple{starlark.None, starlark.String(err.Error())}, nil
+	}
+	return starlark.Tuple{starlark.String(data), starlark.None}, nil
+}
+
+func tryEncode(thread *starlark.Thread, fn *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+	var x starlark.Value
+	if err := starlark.UnpackPositionalArgs(fn.Name(), args, kwargs, 1, &x); err != nil {
+		return starlark.Tuple{starlark.None, starlark.String(err.Error())}, nil
+	}
+
+	encoded, err := itn.EncodeStarlarkJSON(x)
+	if err != nil {
+		return starlark.Tuple{starlark.None, starlark.String(err.Error())}, nil
+	}
+	return starlark.Tuple{starlark.String(encoded), starlark.None}, nil
+}
+
+func tryDecode(thread *starlark.Thread, fn *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+	var s string
+	if err := starlark.UnpackArgs(fn.Name(), args, kwargs, "x", &s); err != nil {
+		return starlark.Tuple{starlark.None, starlark.String(err.Error())}, nil
+	}
+
+	decoded, err := itn.DecodeStarlarkJSON([]byte(s))
+	if err != nil {
+		return starlark.Tuple{starlark.None, starlark.String(err.Error())}, nil
+	}
+	return starlark.Tuple{decoded, starlark.None}, nil
 }
