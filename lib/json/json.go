ef3deb75d9f9aecaee0fa93994fdeb1a8ab8fe70
Hyori 효리 <hyorigo@icloud.com>
2024-11-13 00:53:53 +0800

    for jsonpath raw

--- lib/json/json.go
+++ lib/json/json.go
@@ -4,9 +4,12 @@ package json
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
+	"math"
 	"sync"
 
 	itn "github.com/1set/starlet/dataconv"
+	"github.com/spyzhov/ajson"
 	stdjson "go.starlark.net/lib/json"
 	"go.starlark.net/starlark"
 	"go.starlark.net/starlarkstruct"
@@ -33,6 +36,7 @@ func LoadModule() (starlark.StringDict, error) {
 				"try_encode": starlark.NewBuiltin(ModuleName+".try_encode", tryEncode),
 				"try_decode": starlark.NewBuiltin(ModuleName+".try_decode", tryDecode),
 				"try_indent": starlark.NewBuiltin(ModuleName+".try_indent", tryIndent),
+				"path":       starlark.NewBuiltin(ModuleName+".path", jsonPath),
 			},
 		}
 		for k, v := range stdjson.Module.Members {
@@ -130,4 +134,109 @@ func tryIndent(thread *starlark.Thread, fn *starlark.Builtin, args starlark.Tupl
 		return starlark.Tuple{none, starlark.String(err.Error())}, nil
 	}
 	return starlark.Tuple{starlark.String(buf.String()), none}, nil
+}
+
+// jsonpath performs a JSONPath query on the given JSON data and returns the matching elements.
+// It accepts a data object (Starlark value) and a JSONPath expression string.
+func jsonPath(thread *starlark.Thread, fn *starlark.Builtin, args starlark.Tuple, kwargs []starlark.Tuple) (starlark.Value, error) {
+	var (
+		data     starlark.Value
+		pathExpr string
+	)
+	if err := starlark.UnpackArgs(fn.Name(), args, kwargs, "data", &data, "path", &pathExpr); err != nil {
+		return none, err
+	}
+
+	// Get the JSON data as a string
+	var jb []byte
+	switch v := data.(type) {
+	case starlark.String:
+		jb = []byte(v.GoString())
+	case starlark.Bytes:
+		jb = []byte(v)
+	default:
+		// Serialize the Starlark value to a JSON string
+		js, err := itn.MarshalStarlarkJSON(data, 0)
+		if err != nil {
+			return none, fmt.Errorf("json.path: %w", err)
+		}
+		jb = []byte(js)
+	}
+
+	// Use ajson to parse the JSON data and perform the JSONPath query
+	nodes, err := ajson.JSONPath(jb, pathExpr)
+	if err != nil {
+		return none, fmt.Errorf("json.path: %w", err)
+	}
+
+	// Convert the resulting nodes to Starlark values
+	results := make([]starlark.Value, 0, len(nodes))
+	for _, node := range nodes {
+		val, err := ajsonNodeToStarlarkValue(node)
+		if err != nil {
+			return none, fmt.Errorf("json.path: %w", err)
+		}
+		results = append(results, val)
+	}
+
+	// Return the results as a Starlark list
+	return starlark.NewList(results), nil
+}
+
+// ajsonNodeToStarlarkValue converts an ajson.Node to a Starlark value.
+// It recursively traverses the node tree and constructs the corresponding Starlark values.
+func ajsonNodeToStarlarkValue(node *ajson.Node) (starlark.Value, error) {
+	switch node.Type() {
+	case ajson.Object:
+		dict := &starlark.Dict{}
+		for _, key := range node.Keys() {
+			valNode, err := node.GetKey(key)
+			if err != nil {
+				return nil, err
+			}
+			val, err := ajsonNodeToStarlarkValue(valNode)
+			if err != nil {
+				return nil, err
+			}
+			err = dict.SetKey(starlark.String(key), val)
+			if err != nil {
+				return nil, err
+			}
+		}
+		return dict, nil
+
+	case ajson.Array:
+		// Convert JSON array to Starlark list
+		elements, err := node.GetArray()
+		if err != nil {
+			return nil, err
+		}
+		vals := make([]starlark.Value, len(elements))
+		for i, elem := range elements {
+			val, err := ajsonNodeToStarlarkValue(elem)
+			if err != nil {
+				return nil, err
+			}
+			vals[i] = val
+		}
+		return starlark.NewList(vals), nil
+	case ajson.String:
+		return starlark.String(node.MustString()), nil
+	case ajson.Numeric:
+		// Number value
+		num := node.MustNumeric()
+		if math.Mod(num, 1.0) == 0 {
+			// Integer value
+			return starlark.MakeInt64(int64(num)), nil
+		} else {
+			// Float value
+			return starlark.Float(num), nil
+		}
+	case ajson.Bool:
+		return starlark.Bool(node.MustBool()), nil
+	case ajson.Null:
+		return starlark.None, nil
+	default:
+		return nil, fmt.Errorf("unsupported JSON node type: %v", node.Type())
+	}
 }
